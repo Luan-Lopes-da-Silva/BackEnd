0cf78447909cf8950a90254faece4b4710344661
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-08-14 18:49:12 -0300

    Json answers, postman, first route in express and middlewares

--- Knowing NodeJs/To-do-List/app.js
+++ Knowing NodeJs/To-do-List/app.js
@@ -2,13 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+const log = (req,res,next)=>{
+  console.log(req.body)
+  console.log(Date.now())
+  next()
+}
+
+app.use(log)
+
 app.get('/',(req, res)=>{
-  res.send('Hello World!')
+  res.send(`<h1>Minha lista de tarefas</h1>`)
 })
-app.get('/products',(req,res)=>{
-  res.send('Products')
+
+app.get('/json',(req,res)=>{
+  res.json({name:'Lavar a louça', done:true})
 })
 
 app.listen(port,()=>{
-  console.log(`Example app listening on port ${port}`)
+  console.log('Servidor iniciado')
 })
