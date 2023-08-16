0a88d60c2a3358b7ab88b3fc19c4591cd783434d
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-08-16 14:24:25 -0300

    Conecting mongoDB with node

--- Knowing NodeJs/To-do-List/app.js
+++ Knowing NodeJs/To-do-List/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const checkListRouter = require('./src/routes/checklist')
+require('./config/database')
 
 app.use(express.json())
 app.use('/checklists', checkListRouter)
