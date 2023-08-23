32c7638b331bb7c7cb2dadb714294133e435eedd
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-08-23 15:56:14 -0300

    Put and delete methods

--- Knowing NodeJs/To-do-List/app.js
+++ Knowing NodeJs/To-do-List/app.js
@@ -11,7 +11,7 @@ const path = require('path')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method', {methods:['POST','GET']}))
 
 app.use('/checklists', checkListRouter)
 app.use('/', rootRouter)
